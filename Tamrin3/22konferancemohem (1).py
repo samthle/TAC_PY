f6e28c3aaf9a35a17c4342a2da8eb3ac2ab9b6a2
samthle <157809935+samthle@users.noreply.github.com>
2024-01-26 17:31:19 +0330

    Update 22konferancemohem (1).py
    refactored version

--- Tamrin3/22konferancemohem (1).py
+++ Tamrin3/22konferancemohem (1).py
@@ -1,13 +1,22 @@
-def domain_emails (emails):
+def domain_emails(emails):
     domains = set()
+
     for email in emails:
-        domain = email.split('@')[-1] 
-        domains.add(domain)  
-    return sorted(domains) 
-n = int(input())
-emails = []
-for _ in range(n):
-    email = input()
-    emails.append(email)
-for domain in domain_emails(emails):
-    print(domain)
+        domain = email.split('@')[-1]
+        domains.add(domain)
+
+    return sorted(domains)
+
+def main():
+    n = int(input("Enter the number of emails: "))
+    emails = []
+
+    for _ in range(n):
+        email = input("Enter an email: ")
+        emails.append(email)
+
+    for domain in domain_emails(emails):
+        print(domain)
+
+if __name__ == "__main__":
+    main()
