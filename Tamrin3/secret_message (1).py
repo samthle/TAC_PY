fab5ba5490311f07e8e0e2ed4b43cfc647831875
samthle <157809935+samthle@users.noreply.github.com>
2024-01-26 17:33:39 +0330

    Update secret_message (1).py
    refactored version

--- Tamrin3/secret_message (1).py
+++ Tamrin3/secret_message (1).py
@@ -1,9 +1,13 @@
-v1 = input()
+def create_secret_message(v1):
+    v2 = v1.split()
+    v3 = sorted(v2, key=lambda x: int(x[1:]))
+    secret_message = ''.join([char[0] for char in v3])
+    return secret_message
 
-v2 = v1.split()
+def main():
+    v1 = input("Enter space-separated strings: ")
+    result = create_secret_message(v1)
+    print(result)
 
-v3 = sorted(v2, key=lambda x: int(x[1:]))
-
-secret_message = ''.join([char[0] for char in v3])
-
-print(secret_message)
+if __name__ == "__main__":
+    main()
