2ec96de5e3be7d8a5a0c71f4466a5cd70d9191dd
samthle <157809935+samthle@users.noreply.github.com>
2024-01-26 17:20:02 +0330

    Update tamrin1samtcle.py
    refactored version

--- Tamrin1/tamrin1samtcle.py
+++ Tamrin1/tamrin1samtcle.py
@@ -1,21 +1,18 @@
-#by samtcle
-def numtobinfunc(n):
-  return "{:064b}".format(int(n))
+def num_to_bin_string(num):
+    return "{:064b}".format(num)
 
-a = int(input())
-b = int(input())
+def count_set_bits(binary_string):
+    return sum(1 for bit in binary_string if bit == '1')
 
+def main():
+    a = int(input("Enter the first integer: "))
+    b = int(input("Enter the second integer: "))
 
-anbdif= a ^ b
+    xor_result = a ^ b
+    binary_difference = num_to_bin_string(xor_result)
+    set_bits_count = count_set_bits(binary_difference)
 
-binfinaldif = numtobinfunc(anbdif)
+    print("The number of differing bits is:", set_bits_count)
 
-finalans=0
-
-for i in range(len(binfinaldif)):
-
-  if str(binfinaldif[i])=="1":
-    finalans+=1
-
-
-print(finalans)
+if __name__ == "__main__":
+    main()
